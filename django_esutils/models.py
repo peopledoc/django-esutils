bc7639ce9f5ba9074757bedbeb53e038289f0652
Florent Pigout <florent.pigout@gmail.com>
2015-04-02 22:02:44 +0200

    Enhances update signal.
    Evaluate ids to update first. Then trigger the post_update signal with new qs
    matching these ids to ensure good filtering in the receiver.

--- django_esutils/models.py
+++ django_esutils/models.py
@@ -12,8 +12,17 @@ class ESQuerySet(QuerySet):
     def update(self, *args, **kwargs):
         """Udpates and sends post_update signal with current self.
         """
+        # keep ids to update
+        ids = list(self.values_list('pk', flat=True))
+
+        # do update
         result = super(ESQuerySet, self).update(*args, **kwargs)
-        post_update.send(sender=self.model, queryset=self)
+
+        # call signal with queryset matching the update
+        new_qs = self.model.objects.filter(pk__in=ids)
+        # trigger signal
+        post_update.send(sender=self.model, queryset=new_qs)
+
         return result
 
 
